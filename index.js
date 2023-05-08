805666b827f7afc21e02b2e310f2c2ea675fa049
지영 <toto.joung@gmail.com>
2023-05-08 17:58:24 +0900

    230508

--- index.js
+++ index.js
@@ -38,10 +38,10 @@ var 학교 = {
 //함수에 타입 지정하는 법
 function sayHi(x) {
     if (x) {
-        console.log('안녕하세요 ' + x);
+        console.log("안녕하세요 " + x);
     }
     else {
-        console.log('왜입력안함');
+        console.log("왜입력안함");
     }
 }
 function rundom(x) {
@@ -53,24 +53,24 @@ function 결혼가능하냐(money, house, charm) {
     if (house === true) {
         score += 500;
     }
-    if (charm === '상') {
+    if (charm === "상") {
         score += 100;
     }
     if (score >= 600) {
-        return '결혼가능';
+        return "결혼가능";
     }
 }
-console.log(결혼가능하냐(100, true, '상'));
+console.log(결혼가능하냐(100, true, "상"));
 // 타입 확정하기 Narrowing
 function 내함수(x) {
     var array = [];
-    if (typeof x === 'number') {
+    if (typeof x === "number") {
         array[0] = x;
     }
 }
 function 내함수1(x) {
-    if (typeof x === 'string') {
-        return x + '1';
+    if (typeof x === "string") {
+        return x + "1";
     }
     else {
         return x + 1;
@@ -83,3 +83,44 @@ function 내함수2(x) {
     array[0] = x; //타잎 덮어쓰기
 }
 내함수2(123);
+var 동물들 = "고양이";
+var 동물 = { name: "kim", age: 20 };
+var 출생지역 = "seoul";
+var 출생지역2 = { region: "seoul" };
+출생지역2.region = "busan"; //오브젝트안에 키값을 변경이 가능하다
+var 사람 = {
+    name: "엠버",
+};
+사람.name = "유라"; //에러띄우는거랑 JS실행이랑 상관없음, 에디터 에러일 뿐(참고)
+var position = { x: 10, y: 20 };
+var 숙제 = { color: "블랙", size: 20, position: [1, 2, 3] };
+var 검사 = { name: "kim", phone: 123 };
+//Literal types
+var 접니다;
+function 함수(묵찌빠) {
+    return ["가위"];
+}
+var 자료 = {
+    name: "kim",
+};
+자료.name;
+function 내함수(a) { }
+내함수(자료.name);
+var 함수다 = function (a) {
+    return 3;
+};
+var 회원정보 = {
+    name: 'kim'
+};
+number,
+    changeName;
+(function () { return void ; },
+);
+var cutZero = function (x) {
+    var result = x.replace('0', "");
+    return result;
+};
+function removeDash(x) {
+    var result = x.replace('-', "");
+    return parseFloat(result);
+}
--- index.ts
+++ index.ts
@@ -55,53 +55,127 @@ let 학교: {
 
 //함수에 타입 지정하는 법
 
-
-function sayHi(x? :string ){
+function sayHi(x?: string) {
   if (x) {
-    console.log('안녕하세요 ' + x)
+    console.log("안녕하세요 " + x);
   } else {
-    console.log('왜입력안함')
+    console.log("왜입력안함");
   }
-} 
-
-function rundom(x:number|string):number{
-  return x.toString().length
 }
 
-function 결혼가능하냐(money :number, house :boolean, charm :string) :string|void{
-  let score :number = 0;
+function rundom(x: number | string): number {
+  return x.toString().length;
+}
+
+function 결혼가능하냐(
+  money: number,
+  house: boolean,
+  charm: string
+): string | void {
+  let score: number = 0;
   score += money;
-  if (house === true){
-    score += 500
+  if (house === true) {
+    score += 500;
   }
-  if (charm === '상'){
-    score += 100
+  if (charm === "상") {
+    score += 100;
   }
-  if (score >= 600){
-    return '결혼가능'
-  } 
+  if (score >= 600) {
+    return "결혼가능";
+  }
 }
-console.log(결혼가능하냐(100,true,'상')) 
+console.log(결혼가능하냐(100, true, "상"));
 
 // 타입 확정하기 Narrowing
-function 내함수(x:number|string){
-  let array:number[]=[];
-  if(typeof x === 'number'){
+function 내함수(x: number | string) {
+  let array: number[] = [];
+  if (typeof x === "number") {
     array[0] = x;
   }
 }
-function 내함수1(x:number|string){
-  if(typeof x === 'string'){
-    return x + '1'
+function 내함수1(x: number | string) {
+  if (typeof x === "string") {
+    return x + "1";
   } else {
-    return x + 1
+    return x + 1;
   }
 }
-내함수(123)
+내함수(123);
 // 타입 확정하기 Assertion
-function 내함수2(x:number|string){
-  let array:number[]=[];
+function 내함수2(x: number | string) {
+  let array: number[] = [];
   array[0] = x as number; //타잎 덮어쓰기
-
 }
-내함수2(123)
+내함수2(123);
+
+// type alias-타입 변수
+// 대문자 국룰
+type Animal = string | number | undefined;
+const 동물들: Animal = "고양이";
+
+type Animal2 = { name: string; age: number };
+let 동물: Animal2 = { name: "kim", age: 20 };
+
+const 출생지역 = "seoul";
+const 출생지역2 = { region: "seoul" };
+출생지역2.region = "busan"; //오브젝트안에 키값을 변경이 가능하다
+//하지만 락이 가능함
+
+type Member3 = {
+  readonly name: string;
+  //읽기전용 - 실수로 수정하면 에러남
+  name2?: string;
+  //? string | undeifind
+};
+const 사람: Member3 = {
+  name: "엠버",
+};
+사람.name = "유라"; //에러띄우는거랑 JS실행이랑 상관없음, 에디터 에러일 뿐(참고)
+
+//&연산자로 object타입 합치기
+type PositionX = { x: number };
+type PositionY = { y: number };
+type NewType = PositionX & PositionY; //{x:number, y:number}
+let position: NewType = { x: 10, y: 20 };
+
+type Test = { color?: string; size: number; readonly position: number[] };
+const 숙제: Test = { color: "블랙", size: 20, position: [1, 2, 3] };
+
+type Check = { name: string; phone: number; email?: string };
+const 검사: Check = { name: "kim", phone: 123 };
+
+//Literal types
+let 접니다: "하이" | "공부중";
+
+function 함수(묵찌빠: "가위" | "바위" | "보"): ("가위" | "바위" | "보")[] {
+  return ["가위"];
+}
+
+var 자료 = {
+  name: "kim",
+} as const;
+자료.name;
+
+function 내함수(a: "kim") {}
+내함수(자료.name);
+
+// 함수와 methods에 type alias 지정하는 법
+type 함수타입 = (a: string) => number;
+let 함수다:함수타입 = function(a) {
+  return 3;
+}
+let 회원정보 ={
+  name : 'kim',
+  plusOne(x:number) => number,
+  changeName : ()=>void,
+}
+
+type CutType = (x:string) => string
+let cutZero:CutType=function(x){
+  let result = x.replace('0',"")
+  return result
+}
+function removeDash(x:string):number{
+  let result = x.replace('-',"");
+  return parseFloat(result)
+}
